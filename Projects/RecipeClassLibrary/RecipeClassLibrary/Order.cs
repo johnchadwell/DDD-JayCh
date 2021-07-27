86e358b048060ddbed60965cb66f355bfcd62b2c
john <40181510+johnchadwell@users.noreply.github.com>
2021-07-27 08:06:21 -0500

    Add files via upload

---
+++ Projects/RecipeClassLibrary/RecipeClassLibrary/Order.cs
@@ -0,0 +1,22 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeClassLibrary.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Employee Employee { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public double TotalCost()
+        {
+            return 0.0;
+        }
+
+    }
+}
